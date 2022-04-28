29dbbc95103aaab8a0f5674362cc258a9597bd94
Satrey <satrey.mail@gmail.com>
2022-04-28 16:58:54 +0500

    Created task manager
    Next chapter Computed property

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,69 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <task-input v-on:signalNewTask="addNewTask" />
+      
+      <v-card class="mx-auto" max-width="800">
+        
+        <v-list-item class="pt-6 pb-6 pl-4 pr-4 tasklist d-flex" v-for="(item, index) in list" :key='index'> 
+          <v-list-item-header>
+            {{ item.finishedAt }} : {{ item.closed }}
+            <v-list-item-title class="p-4 mb-2">{{index + 1}} : {{item.task}} : {{ item.published }}</v-list-item-title>
+            <v-list-item-subtitle>{{ description }}</v-list-item-subtitle>          
+          </v-list-item-header>          
+          <v-checkbox hide-details color="red" :checked="!!item.closed" @input="changeTaskStatus(index)"></v-checkbox>
+        </v-list-item>
+      </v-card>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskInput from './components/TaskInput.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    TaskInput,
   },
 
   data: () => ({
-    //
+    list: [],
+    description: 'Также как сплочённость команды профессионалов играет важную роль в формировании направлений прогрессивного развития.',
+    chackStatus: false,
   }),
+
+  methods: {
+    addNewTask(task) {
+      console.log('New task recieved from TaskInput component : ', task);
+      this.list.push({task: task,
+                      published: Date.now(),
+                      finishedAt: undefined,
+                      closed : false,  
+                      });
+      console.log(this.list);
+    },
+    changeTaskStatus(index){
+      console.log('Статус : ',index)
+      const task = this.list[index];
+      if(task.closed){
+          task.finishedAt = undefined;
+          task.closed = false;
+      }
+      else {
+        task.finishedAt = Date.now();
+        task.closed = true;
+      }
+      
+    },
+  },
 }
 </script>
+
+<style scoped>
+.tasklist:hover{
+  background-color: rgba(194, 194, 194, 0.363);
+
+}
+</style>
---
+++ src/components/TaskInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-card class="mx-auto mt-10 mb-10 pb-5" max-width="800">
+      <v-card-title class="p-3">
+        <h2>Task : {{ task }}</h2>
+      </v-card-title>
+      <div class="input_row">
+        <v-col>
+          <v-text-field hide-details v-model="task" label="Contained" variant="contained" clearable clear-icon="mdi-delete">
+          </v-text-field>
+        </v-col>
+        <v-card-actions>
+          <v-btn variant="text" color="teal-accent-4" @click="addTask">
+            Добавить
+          </v-btn>
+        </v-card-actions>
+      </div>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TaskInput',
+  data() {
+    return {
+      task: '',
+    }
+  },
+  methods: {
+    addTask(){
+      this.$emit('signalNewTask', this.task);
+      console.log(this.task, 'added');
+      this.task = '';
+    },
+  },
+
+}
+</script>
+<style lang="css">
+  .input_row {
+    display: flex;
+  }
+</style>
