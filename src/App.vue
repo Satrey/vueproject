235e4519533c81e6ca4585e9c876bd03efedef49
Satrey <satrey.mail@gmail.com>
2022-04-29 15:21:56 +0500

    Added filter sorting

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,18 @@
       <task-input v-on:signalNewTask="addNewTask" />
       
       <v-card class="mx-auto" max-width="800">
-        
-        <v-list-item class="pt-6 pb-6 pl-4 pr-4 tasklist d-flex" v-for="(item, index) in list" :key='index'> 
+        <v-container class="d-flex justify-start  task_title" v-if="list.length > 0">
+          <v-checkbox class="" hide-details color="green" label="Hide Done" v-model="hideDone"></v-checkbox>
+          <v-checkbox class="" hide-details color="green" label="Reverse list" v-model="reverse"></v-checkbox>
+          <v-checkbox class="" hide-details color="green" label="Sort by ID" v-model="sortById"></v-checkbox>
+        </v-container>
+        <v-list-item class="pt-6 pb-6 pl-4 pr-4 tasklist d-flex justify-space-between mb-6" v-for="(item, index) in displayList" :key='index'> 
           <v-list-item-header>
-            {{ item.finishedAt }} : {{ item.closed }}
-            <v-list-item-title class="p-4 mb-2">{{index + 1}} : {{item.task}} : {{ item.published }}</v-list-item-title>
-            <v-list-item-subtitle>{{ description }}</v-list-item-subtitle>          
+            <v-list-item-title class="p-4 mb-2">{{item.id}} : {{item.task}} : {{ formatDate(item.published) }} </v-list-item-title>
+            <v-list-item-subtitle>{{ item.description }}</v-list-item-subtitle>          
+            <v-list-item-subtitle v-if="item.closed">Завершена : {{ formatDate(item.finishedAt) }}</v-list-item-subtitle>
           </v-list-item-header>          
-          <v-checkbox hide-details color="red" :checked="!!item.closed" @input="changeTaskStatus(index)"></v-checkbox>
+          <v-checkbox class="justify-end mb-6" hide-details color="red" v-model="item.closed" @input="changeTaskStatus(item.id)"></v-checkbox>
         </v-list-item>
       </v-card>
     </v-main>
@@ -30,38 +34,83 @@ export default {
 
   data: () => ({
     list: [],
-    description: 'Также как сплочённость команды профессионалов играет важную роль в формировании направлений прогрессивного развития.',
-    chackStatus: false,
+    hideDone: false,
+    reverse : false,
+    sortById: true, 
   }),
 
   computed: {
+    baseList(){
+      return [...this.list]
+          .map((t, index) => ({
+            ...t,
+            id: index + 1
+          }));
+    },
+
+    filteredList(){
+      return this.hideDone
+          ?  [... this.baseList]
+          .filter(t => !t.finishedAt)
+          : this.baseList;
+    },
+
+    sortedList(){
+      return [...this.filteredList]
+          .sort((a,b) => (this.sortById ? b.id - a.id : (a.finishedAt || 0) - (b.finishedAt || 0)));
+    },
+
     displayList(){
-      return this.list();
+      const taskList = [...this.sortedList];
+      return this.reverse ? taskList.reverse() : taskList;
     },
   },
 
   methods: {
-    addNewTask(task) {
+    addNewTask(task, description) {
       console.log('New task recieved from TaskInput component : ', task);
       this.list.push({task: task,
+                      description: description,
                       published: Date.now(),
                       finishedAt: undefined,
                       closed : false,  
                       });
       console.log(this.list);
     },
+    
     changeTaskStatus(index){
-      console.log('Статус : ',index)
-      const task = this.list[index];
+      
+      const task = this.list[index - 1];
       if(task.closed){
           task.finishedAt = undefined;
           task.closed = false;
+          console.log('Статус : ',task.closed)
       }
       else {
         task.finishedAt = Date.now();
         task.closed = true;
-      }
-      
+        console.log(task.closed)
+        console.log(this.list)
+      }  
+    },
+
+    formatDate(value){
+      if(!value) return '';
+      if(typeof value !== 'number') return value;
+
+      const browserLocale = navigator.languages && navigator.languages.length
+            ? navigator.languages[0]
+            : navigator.language;
+      const intDateTime   = new Intl.DateTimeFormat(browserLocale,
+      {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      });
+      return intDateTime.format(new Date(value));
     },
   },
 }
@@ -71,5 +120,12 @@ export default {
 .tasklist:hover{
   background-color: rgba(194, 194, 194, 0.363);
 
+}
+
+.task_title{
+  background-color: rgb(102, 0, 219);
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  color: white;
 }
 </style>
--- src/components/TaskInput.vue
+++ src/components/TaskInput.vue
@@ -2,19 +2,21 @@
   <div>
     <v-card class="mx-auto mt-10 mb-10 pb-5" max-width="800">
       <v-card-title class="p-3">
-        <h2>Task : {{ task }}</h2>
+          <h2>Task : {{ task }}</h2>
       </v-card-title>
-      <div class="input_row">
-        <v-col>
-          <v-text-field hide-details v-model="task" label="Contained" variant="contained" clearable clear-icon="mdi-delete">
+      <!-- <div class="input_row d-flex justify-space-between"> -->
+        <v-col >
+          <v-text-field class="mb-4" hide-details v-model="task" label="Contained" variant="contained" clearable clear-icon="mdi-delete">
+          </v-text-field>
+          <v-text-field hide-details v-model="description" label="Contained" variant="contained" clearable clear-icon="mdi-delete">
           </v-text-field>
         </v-col>
-        <v-card-actions>
-          <v-btn variant="text" color="teal-accent-4" @click="addTask">
+        <v-container class="d-flex justify-end me-1">
+          <v-btn color="primary" @click="addTask">
             Добавить
           </v-btn>
-        </v-card-actions>
-      </div>
+        </v-container>
+      <!-- </div> -->
     </v-card>
   </div>
 </template>
@@ -24,13 +26,17 @@ export default {
   data() {
     return {
       task: '',
+      description: '',
     }
   },
   methods: {
     addTask(){
-      this.$emit('signalNewTask', this.task);
+      this.$emit('signalNewTask', this.task, this.description);
       console.log(this.task, 'added');
+      console.log(this.description, 'added');
       this.task = '';
+      this.description = '';
+
     },
   },
 
